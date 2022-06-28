2cb72945fc66065d3972eafedffafe4f2be1a8f5
L Prather <63510911+lprather@users.noreply.github.com>
2022-06-28 13:17:42 -0700

    Update __init__.py
    route for timeline.html

--- app/__init__.py
+++ app/__init__.py
@@ -43,6 +43,14 @@ def hobbies():
 @app.route("/travel")
 def travel():
     return render_template('travel.html')
+  
+@app.route('/timeline')
+def timeline():
+    posts = [
+        model_to_dict(p)
+        for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+    ]
+	  return render_template('timeline.html', title="Timeline", posts=posts)
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
