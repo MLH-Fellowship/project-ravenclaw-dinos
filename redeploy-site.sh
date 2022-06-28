d1fc0ee0de15ac0a9a5d0e4a53ed8ba50db14d35
L Prather <63510911+lprather@users.noreply.github.com>
2022-06-28 14:27:07 -0700

    Update redeploy-site.sh
    implement use of services

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,11 +1,7 @@
-tmux kill-server
 cd /root/project-ravenclaw-dinos
 git fetch && git reset origin/main --hard
 source python3-virtualenv/bin/activate
 pip3 install -r requirements.txt
-TSESH="site-autodeploy"
-WIN=0
-tmux new -d -s $TSESH
-tmux send-keys -t $TSESH:$WIN 'export FLASK_ENV=development' Enter
-tmux send-keys -t $TSESH:$WIN 'flask run --host=0.0.0.0' Enter
-exit 0
+RUN="flask run --host=0.0.0.0"
+systemctl daemon-reload
+systemctl restart myportfolio
